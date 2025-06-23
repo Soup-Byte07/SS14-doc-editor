f81fcc47dda357e8142a2b7976aaf0b813f9055b
Soup-Byte07 <soupbyte30@gmail.com>
2025-06-23 04:34:20 -0400

    Fixes, and added dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const REPO_NAME = 'SS14-doc-editor'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${REPO_NAME}/`,
+  build: {
+    outDir: 'dist'
+  }
 })
